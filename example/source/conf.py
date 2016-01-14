45d2efe876ec2a95813ecb7652af8f914aab230a
Nathan Goldbaum <ngoldbau@illinois.edu>
2016-01-14 16:39:30 -0600

    Using yt's sphinx bootstrap theme for example

--- example/source/conf.py
+++ example/source/conf.py
@@ -101,12 +101,19 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+import sphinx_bootstrap_theme
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = dict(
+    bootstrap_version="3",
+    bootswatch_theme="readable",
+    navbar_sidebarrel=False,
+    globaltoc_depth=2,
+)
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
