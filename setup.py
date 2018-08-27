ad764bc465c7a0572ce46142ea1bc09092c524c0
Nathan Goldbaum <ngoldbau@illinois.edu>
2018-08-27 12:02:07 -0400

    add readme to pypi package, update to 0.3.1

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name="RunNotebook",
-    version="0.3.0",
+    version="0.3.1",
     author="Nathan Goldbaum",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author_email="nathan12343@gmail.com",
     url="https://github.com/ngoldbaum/RunNotebook",
     packages=["RunNotebook"],
